bd51493793490a241eb057086d1429edee6e6d9f
ClausFT <clausfyhn@gmail.com>
2014-10-21 10:54:38 +0200

    Initial commit

---
+++ Windows-Programming.Model/Edge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Programming.Model
+{
+    class Edge : NotifyBase
+    {
+        // Properties.
+        private Node endA;
+        private Node endB;
+        public Node EndA { get { return endA; } set { endA = value; NotifyPropertyChanged("EndA"); } }
+        public Node EndB { get { return endB; } set { endB = value; NotifyPropertyChanged("EndB"); } }
+    }
+}
---
+++ Windows-Programming/View/ClassUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Windows_Programming.View
+{
+    /// <summary>
+    /// Interaction logic for ClassUserControl1.xaml
+    /// </summary>
+    public partial class ClassUserControl1 : UserControl
+    {
+        public ClassUserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
