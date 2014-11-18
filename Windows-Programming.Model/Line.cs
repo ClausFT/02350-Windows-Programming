b8d13d7495bddefd3c29d028442789ed1829a1f8
bmalmh <bmalmh@gmail.com>
2014-11-18 11:55:31 +0100

    github fix

--- Windows-Programming.Model/Line.cs
+++ Windows-Programming.Model/Line.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Windows_Programming.Model.Enums;
+using Windows_Programming.Model.Utils;
 
 namespace Windows_Programming.Model
 {
@@ -14,34 +16,91 @@ namespace Windows_Programming.Model
         //  but in this case additional work has to be done when the property is changed, 
         //  which is to raise an INotifyPropertyChanged event that notifies the View (GUI) that this model property has changed, 
         //  so the graphical representation can be updated.
+
         private Shape from;
         public Shape From { get { return from; } set { from = value; NotifyPropertyChanged("From"); } }
-        // This corresponds to the following in Java (can also be done like this in .NET):
-
-        // private Shape from;
-        // 
-        // public Shape getFrom(){
-        //   return from;
-        // }
-        //
-        // public void setFrom(Shape value){
-        //   from = value;
-        //   NotifyPropertyChanged("From");
-        // }
 
         private Shape to;
         public Shape To { get { return to; } set { to = value; NotifyPropertyChanged("To"); } }
-        // This corresponds to the following in Java (can also be done like this in .NET):
 
-        // private Shape to;
-        // 
-        // public Shape getTo(){
-        //   return to;
-        // }
-        //
-        // public void setTo(Shape value){
-        //   to = value;
-        //   NotifyPropertyChanged("To");
-        // }
+        private int _fromX;
+        public int FromX { get { return _fromX; } set { _fromX = value; NotifyPropertyChanged("FromX"); } }
+
+        private int _fromY;
+        public int FromY { get { return _fromY; } set { _fromY = value; NotifyPropertyChanged("FromY"); } }
+
+        private int _toX;
+        public int ToX { get { return _toX; } set { _toX = value; NotifyPropertyChanged("ToX"); } }
+
+        private int _toY;
+        public int ToY { get { return _toY; } set { _toY = value; NotifyPropertyChanged("ToY"); } }
+
+        public RelationTypes Type { get; set; }
+
+        // Sets the coordinates to the shortest line between the two shapes
+        public void SetShortestLine()
+        {
+            int[,] coord = { { From.X, From.Y + From.Height, To.X, To.Y+1 }, //bottom-top
+                             { From.X, From.Y+1, To.X, To.Y + To.Height+1 }, //top-bottom
+                             { From.X + From.Width, From.Y, To.X+1, To.Y },  //right-left
+                             { From.X+1, From.Y, To.X + To.Width+1, To.Y } };  //left-right
+
+            int widthUnit = From.Width / 25;
+            int heightUnit = From.Height / 25;
+            int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+            double minLength = -1;
+            const int margin = 5;
+
+            //This loop calculates shortest line from top-bottom/bottom-top (Y axis is constant)
+            for (int i = 0; i < 2; i++) //For each set top-bottom coordinat (first two elements in array)
+            {
+                for (int k = margin; k < From.Width; k = k + widthUnit)
+                {
+                    for (int j = margin; j < From.Width; j = j + widthUnit)
+                    {
+                        //Calculate the length of the line
+                        double lineLength = Compute.LineLength(coord[i, 0] + k, coord[i, 1], coord[i, 2] + j, coord[i, 3]);
+
+                        //If current line is the shortest so far, save length and coordinates
+                        if (minLength == -1 || lineLength <= minLength)
+                        {
+                            minLength = lineLength;
+                            x1 = coord[i, 0] + k;
+                            y1 = coord[i, 1];
+                            x2 = coord[i, 2] + j;
+                            y2 = coord[i, 3];
+                        }
+                    }
+                }
+            }
+
+            //This loop calculates shortest line from left-right/right-left (X axis is constant)
+            for (int i = 2; i < 4; i++) //For each set left-right coordinat (last two elements in array)
+            {
+                for (int k = margin; k < From.Height; k = k + heightUnit)
+                {
+                    for (int j = margin; j < From.Height; j = j + heightUnit)
+                    {
+                        //Calculate the length of the line
+                        double lineLength = Compute.LineLength(coord[i, 0], coord[i, 1] + k, coord[i, 2], coord[i, 3] + j);
+
+                        //If current line is the shortest so far, save length and coordinates
+                        if (minLength == -1 || lineLength <= minLength)
+                        {
+                            minLength = lineLength;
+                            x1 = coord[i, 0];
+                            y1 = coord[i, 1] + k;
+                            x2 = coord[i, 2];
+                            y2 = coord[i, 3] + j;
+                        }
+                    }
+                }
+            }
+
+            FromX = x1;
+            FromY = y1;
+            ToX = x2;
+            ToY = y2;
+        }
     }
 }
